b1260b5b9f52112c127822f372e870ac5775c0a0
leticia.verta <leticia.verta@f023dc34-385b-d300-6718-75d3a29cf39a>
2010-04-15 17:40:44 +0000

    Interface da camada de trabalho.

---
+++ Business/Base/IBL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL;
+
+namespace Business
+{
+    public abstract class IBL<T, DAL> where DAL : IDAL<T>, new()
+    {
+        public void salvar(T item)
+        {
+            new DAL().salvar(item);
+        }
+
+        public List<T> listarTodos()
+        {
+            return new DAL().listarTodos();
+        }
+
+        public T buscarPorId(T item)
+        {
+            return new DAL().buscarPorId(item);
+        }
+
+        public List<T> buscarPorFiltro(Dictionary<String, object> filtros)
+        {
+            return new DAL().buscarPorFiltro(filtros);
+        }
+    }
+}
