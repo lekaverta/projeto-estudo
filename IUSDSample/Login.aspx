2fc6eaf728037bdb89ad9ec3090548ef887e2250
rafamerlin <rafamerlin@f023dc34-385b-d300-6718-75d3a29cf39a>
2010-04-29 03:30:32 +0000

    Adicionado sistema de Login, porém, ainda não faz acesso ao banco de dados. Usuário e senha padrão "admin".

---
+++ IUSDSample/Login.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Shared/SiteDefault.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="IUSDSample.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphConteudo" runat="server">
+    <br />
+    <br />
+    <table>
+       <tr>
+          <td>Usuário: (Admin)</td>
+          <td><input id="txtUserName" type="text" runat="server"></td>
+          <td><ASP:RequiredFieldValidator ControlToValidate="txtUserName"
+               Display="Static" ErrorMessage="*" runat="server" 
+               ID="vUserName" /></td>
+       </tr>
+       <tr>
+          <td>Senha: (Admin)</td>
+          <td><input id="txtUserPass" type="password" runat="server"></td>
+          <td><ASP:RequiredFieldValidator ControlToValidate="txtUserPass"
+              Display="Static" ErrorMessage="*" runat="server" 
+              ID="vUserPass" />
+          </td>
+       </tr>
+       <tr>
+          <td>Permanecer Logado?</td>
+          <td><ASP:CheckBox id="chkPersistCookie" runat="server" autopostback="false" /></td>
+          <td></td>
+       </tr>
+    </table>
+    <asp:Button ID="cmdLogin" runat="server" Text="Logon" Font-Names="Verdana" 
+    onclick="cmdLogin_Click"/><p></p>
+    <asp:Label id="lblMsg" ForeColor="red" Font-Name="Verdana" Font-Size="10" runat="server" />
+</asp:Content>
---
+++ IUSDSample/Login.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using IUSDSample.Util;
+
+namespace IUSDSample
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        // Merlin - Usei como exemplo para criar o login o seguinte tuto: http://support.microsoft.com/kb/301240
+
+        #region Parametros
+        private bool ErroLogin
+        {
+            get
+            {
+                return (AspHelper.ParamPaginaInt("Erro") > 0 ? true : false);
+            }
+        }
+        #endregion
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (this.ErroLogin)
+                    this.lblMsg.Text = "Login ou Senha Inválidos!";
+            }
+        }
+
+        public bool ValidarUsuario(string Usuario, string Senha)
+        {
+            if (Usuario == "admin" && Senha == "admin")
+                return true;
+            else
+                return false;
+        }
+
+        protected void cmdLogin_Click(object sender, EventArgs e)
+        {
+            if (ValidarUsuario(txtUserName.Value, txtUserPass.Value))
+                FormsAuthentication.RedirectFromLoginPage(txtUserName.Value,chkPersistCookie.Checked);
+            else
+                Response.Redirect("login.aspx?Erro=1", true);
+        }
+    }
+}
