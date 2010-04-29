166eeebd21f86f669325af54bb26b58e99954171
rafamerlin <rafamerlin@f023dc34-385b-d300-6718-75d3a29cf39a>
2010-04-29 03:30:32 +0000

    Adicionado sistema de Login, porém, ainda não faz acesso ao banco de dados. Usuário e senha padrão "admin".

---
+++ trunk/IUSDSample/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Security;
+
+namespace IUSDSample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.cmdSignOut.ServerClick += new System.EventHandler(this.cmdSignOut_ServerClick);
+            
+            //if (!this.IsPostBack)
+            //    this.Form.DefaultFocus = txtUsuario.ClientID;
+
+        }
+
+        private void cmdSignOut_ServerClick(object sender, System.EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            Response.Redirect("login.aspx", true);
+        }
+
+    }
+}
