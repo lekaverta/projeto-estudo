c6da4cc074e6ba508430d39070db8dd4af7158bd
leticia.verta <leticia.verta@f023dc34-385b-d300-6718-75d3a29cf39a>
2010-10-06 01:55:32 +0000

    Menu de imagens dinamico (simulação menu marcas)

--- IUSDSample/Shared/SiteDefault.Master.cs
+++ IUSDSample/Shared/SiteDefault.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,7 +13,41 @@ namespace IUSDSample
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                carregarMenuItens();
+        }
 
+        private void carregarMenuItens()
+        {
+            var simulacaoRegistros = new Dictionary<int, string>();
+            simulacaoRegistros.Add(1, "/imagens/Flor-de-lotus.jpg");
+            simulacaoRegistros.Add(5, "/imagens/Flor-de-lotus.jpg");
+            simulacaoRegistros.Add(6, "/imagens/Flor-de-lotus.jpg");
+            simulacaoRegistros.Add(9, "/imagens/Flor-de-lotus.jpg");
+            simulacaoRegistros.Add(15, "/imagens/Flor-de-lotus.jpg");
+
+            rptMenuImagens.DataSource = simulacaoRegistros;
+            rptMenuImagens.DataBind();
+        }
+
+        protected void btnItemMenu_Click(object sender, ImageClickEventArgs e)
+        {
+            var btnItemMenu = (ImageButton)sender;
+            var idSelecionado = btnItemMenu.CommandArgument;
+            Session["menuSelecionado"] = idSelecionado;
+            ScriptManager.RegisterClientScriptBlock(this.Page, this.Page.GetType(), "MENU",
+                                                    String.Format("javascript:alert('Id do menu selecionado: {0}');",
+                                                                  idSelecionado), true);
+        }
+
+        protected void rptMenuImagens_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                var btnItemMenu = (ImageButton) e.Item.FindControl("btnItemMenu");
+                btnItemMenu.ImageUrl = DataBinder.Eval(e.Item.DataItem, "value").ToString();
+                btnItemMenu.CommandArgument = DataBinder.Eval(e.Item.DataItem, "key").ToString();
+            }
         }
     }
 }
